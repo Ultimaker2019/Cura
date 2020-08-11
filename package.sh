6dec03cb16ca2fb1993264c871e731116079678e
edwardfang1945 <edwardfang1945@outlook.com>
2020-08-11 16:52:53 +0800

    Change build_name to 20.08.10_X5_Dual

--- package.sh
+++ package.sh
@@ -22,7 +22,7 @@ BUILD_TARGET=${1:-none}
 ##Do we need to create the final archive
 ARCHIVE_FOR_DISTRIBUTION=1
 ##Which version name are we appending to the final archive
-export BUILD_NAME=20.08.04_X5_Dual
+export BUILD_NAME=20.08.10_X5_Dual
 TARGET_DIR=Cura-${BUILD_NAME}-${BUILD_TARGET}
 
 ##Which versions of external programs to use
